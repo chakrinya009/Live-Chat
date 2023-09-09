525f8619f0b936da462a19e09c7d68447e8974e2
chakrinya <chakrinyabalabadra@gmail.com>
2023-09-09 10:59:28 +0530

    socket

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
-const {Server}=require('socket.io')
+const { Server } = require("socket.io");
 
-const io=new Server(8000)
+const io = new Server(8000, {
+  cors: true,
+});
 
-io.on('connection',socket=>{
-    console.log('Socket connected',socket.io);
-})
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log("Socket connected", socket.id);
+  socket.on("room:join", (data) => {
+    console.log(data);
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+});
